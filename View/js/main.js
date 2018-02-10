c09239b359a917c2902d93cd006146f1d83a07f7
Muhammad Hasan Alasady <muhmdhsn313@gmail.com>
2018-02-10 08:51:22 -0500

    Edite on index.html page, add new logo

--- View/js/main.js
+++ View/js/main.js
@@ -1,17 +1,2 @@
-var s1111 = document.getElementById("1111"),
-    s2222 = document.getElementById("2222"),
-    onIt  = document.getElementById("onit")
 
-    function oncros1(){
-
-        onIt.innerHTML = "<div id=\"hack1\">This is fales, in 1111</div>";
-
-    }
-    if(onmouseover.s1111){
-
-        oncros1();
-
-    }else{
-        onIt.innerHTML=" ";
-    }
 
